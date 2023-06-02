1218f2fc087e6ab8568523f82cd5872d2308ebdc
yangyeongwon <yyeongwon123@gmail.com>
2023-06-02 11:23:47 +0900

    20230602_yyw add fly.toml add fly.yml add front-end source code

--- src/main/java/com/toyproject/sns/sns/SnsApplication.java
+++ src/main/java/com/toyproject/sns/sns/SnsApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
+//데이터베이스 연결 안되어 run 실행 오류 exclude 처리
 @SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
 public class SnsApplication {
 
